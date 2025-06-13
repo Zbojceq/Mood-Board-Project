f77ce4eb90f006a80b6eaf2067511183e39e77df
Zbojceq PC <zbojceq@gmail.com>
2025-06-13 15:55:52 +0200

    Main_code_work 1

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route('/')
 def hello_world():
  return 'Hello, World!'
+
+@app.route('/start')
+def start():
+ return render_template('start.html')
+
+@app.route('/login')
+def login():
+ return render_template('login.html')
